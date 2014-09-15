9443d1e6200f38c297b9d9fb702ccfa18834d309
Andy Buntine <bunts@hhd.com.au>
2014-09-15 14:53:22 +1000

    Started fleshing out initial design

--- lib/ruby_swift.rb
+++ lib/ruby_swift.rb
@@ -1,2 +1,18 @@
 class RubySwift
+
+  def initialize(password)
+    @password = password
+    @client   = Savon.client(endpoint: "http://suite.peoplelogic.com.au/server/includes/apis/soap.php",
+                             namespace: "ns1",
+                             convert_request_keys_to: :none)
+  end
+
+  def read_person(email)
+  end
+
+ private
+
+  def soap_request(message)
+  end
+
 end
